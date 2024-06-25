174d5f9374f4d15955ea9b851566d415dd58b896
unique <1875760364@qq.com>
2024-06-26 00:09:31 +0800

    禁止主题切换

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,24 +1,41 @@
 import { useGlobal } from '@/lib/global'
 import LANGS from '@/lib/lang'
+import { getQueryParam } from '@/lib/utils'
+import { THEMES } from '@/themes/theme'
 import { useRouter } from 'next/router'
 import { useState } from 'react'
+import DarkModeButton from './DarkModeButton'
 import { Draggable } from './Draggable'
-
 /**
  *
  * @returns 主题切换
  */
 const ThemeSwitch = () => {
-  const { lang, changeLang, locale } = useGlobal()
+  const { theme, lang, changeLang, locale, isDarkMode, toggleDarkMode } =
+    useGlobal()
   const router = useRouter()
+  const currentTheme = getQueryParam(router.asPath, 'theme') || theme
+  //   const currentLang = getQueryParam(router.asPath, 'lang') || lang
   const [isLoading, setIsLoading] = useState(false)
 
-  // 语言切换处理函数
-  const onLangSelectChange = e => {
-    document.ontouchmove = document.ontouchend = document.onmousemove = document.onmouseup = null
-    const newLang = e.target.value
-    changeLang(newLang)
-  }
+ // 重定向到指定URL
+ const redirectToCustomURL = () => {
+  setIsLoading(true)
+  const targetURL = 'https://blog.uniquecharm.online/'
+  window.location.href = targetURL
+}
+
+const onThemeSelectChange = e => {
+  document.ontouchmove = document.ontouchend = document.onmousemove = document.onmouseup = null
+  redirectToCustomURL()
+}
+
+const onLangSelectChange = e => {
+  document.ontouchmove = document.ontouchend = document.onmousemove = document.onmouseup = null
+  const newLang = e.target.value
+  changeLang(newLang)
+  redirectToCustomURL()
+}
 
   return (
     <>
@@ -27,7 +44,40 @@ const ThemeSwitch = () => {
           id='draggableBox'
           style={{ left: '0px', top: '80vh' }}
           className='fixed group hover:scale-105 transition-all space-y-2 overflow-hidden z-50 p-3 flex flex-col items-start dark:text-white bg-white dark:bg-black rounded-xl shadow-lg '>
-          
+          {/* 主题切换按钮 */}
+          <div className='text-sm flex items-center group-hover:w-32 transition-all duration-200'>
+            <i className='fa-solid fa-palette w-5' />
+            <div className='w-0 group-hover:w-24 transition-all duration-200 overflow-hidden'>
+              <label htmlFor='themeSelect' className='sr-only'>
+                {locale.COMMON.THEME}
+              </label>
+              <select
+                id='themeSelect'
+                value={currentTheme}
+                onChange={onThemeSelectChange}
+                name='themes'
+                className='pl-1 bg-gray-50 dark:bg-black appearance-none outline-none dark:text-white uppercase cursor-pointer'>
+                {THEMES?.map(t => {
+                  return (
+                    <option key={t} value={t}>
+                      {t}
+                    </option>
+                  )
+                })}
+              </select>
+            </div>
+          </div>
+
+          {/* 深色按钮 */}
+          <div className='text-sm flex items-center w-0 group-hover:w-32 transition-all duration-200'>
+            <DarkModeButton />
+            <div
+              onClick={toggleDarkMode}
+              className='cursor-pointer w-0 group-hover:w-24 transition-all duration-200 overflow-hidden whitespace-nowrap pl-1 h-auto'>
+              {isDarkMode ? locale.MENU.DARK_MODE : locale.MENU.LIGHT_MODE}
+            </div>
+          </div>
+
           {/* 翻译按钮 */}
           <div className='text-sm flex items-center group-hover:w-32 transition-all duration-200'>
             <i className='fa-solid fa-language w-5' />
