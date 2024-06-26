15cd19b5e753e42d1e6460470d8bace3297e5bbc
unique <1875760364@qq.com>
2024-06-26 19:36:21 +0800

    ‘123456’

--- components/aiDigest.js
+++ components/aiDigest.js
@@ -1,87 +1,102 @@
-if (!window.hasOwnProperty("aiExecuted")) {
-	console.log(`%cPost-Summary-AI 文章摘要AI生成工具:%chttps://github.com/qxchuckle/Post-Summary-AI%c`, "border:1px #888 solid;border-right:0;border-radius:5px 0 0 5px;padding: 5px 10px;color:white;background:#4976f5;margin:10px 0", "border:1px #888 solid;border-left:0;border-radius:0 5px 5px 0;padding: 5px 10px;", "");
-	window.aiExecuted = "chuckle";
+if (!Object.prototype.hasOwnProperty.call(window, "aiExecuted")) {
+    console.log(`%cPost-Summary-AI 文章摘要AI生成工具:%chttps://github.com/qxchuckle/Post-Summary-AI%c`, 
+        "border:1px #888 solid;border-right:0;border-radius:5px 0 0 5px;padding: 5px 10px;color:white;background:#4976f5;margin:10px 0", 
+        "border:1px #888 solid;border-left:0;border-radius:0 5px 5px 0;padding: 5px 10px;", 
+        ""
+    );
+    window.aiExecuted = "chuckle";
 }
-function ChucklePostAI(AI_option) {
-	MAIN(AI_option);
-	if (AI_option.pjax) {
-		document.addEventListener('pjax:complete', () => {
-			setTimeout(() => {
-				MAIN(AI_option);
-			}, 0);
-		});
-	}
-	function MAIN(AI_option) {
-		// 如果有则删除
-		const box = document.querySelector(".post-ai");
-		if (box) {
-			box.parentElement.removeChild(box);
-		}
-		const currentURL = window.location.href;
-		// 排除页面
-		if (AI_option.eliminate && AI_option.eliminate.length && AI_option.eliminate.some(item => currentURL.includes(item))) {
-			console.log("Post-Summary-AI 已排除当前页面(黑名单)");
-			return;
-		}
-		if (AI_option.whitelist && AI_option.whitelist.length && !AI_option.whitelist.some(item => currentURL.includes(item))) {
-			console.log("Post-Summary-AI 已排除当前页面(白名单)");
-			return;
-		}
-		// 获取挂载元素，即文章内容所在的容器元素
-		let targetElement = "";
-		// 若el配置不存在则自动获取，如果auto_mount配置为真也自动获取
-		if (!AI_option.auto_mount && AI_option.el) {
-			targetElement = document.querySelector(AI_option.el ? AI_option.el : '#post #article-container');
-		} else {
-			targetElement = getArticleElements();
-		}
-		// 获取文章标题，默认获取网页标题
-		const post_title = document.querySelector(AI_option.title_el) ? document.querySelector(AI_option.title_el).textContent : document.title;
-		if (!targetElement) {
-			return;
-		};
-		const aiInterface = {
-			name: "AI摘要",
-			introduce: "我是文章辅助AI, 用于生成本文简介。",
-			version: "GPT",
-			...AI_option.aiInterface
-		}
-		insertCSS(); // 插入css
-		// 插入html结构
-		const post_ai_box = document.createElement('div');
-		post_ai_box.className = 'post-ai';
-		post_ai_box.setAttribute('id', 'post-ai');
-		targetElement.insertBefore(post_ai_box, targetElement.firstChild);
-		post_ai_box.innerHTML = `<div class="ai-title">
-		  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="21px" height="21px" viewBox="0 0 48 48">
-		  <g id="&#x673A;&#x5668;&#x4EBA;" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><path d="M34.717885,5.03561087 C36.12744,5.27055371 37.079755,6.60373651 36.84481,8.0132786 L35.7944,14.3153359 L38.375,14.3153359 C43.138415,14.3153359 47,18.1768855 47,22.9402569 L47,34.4401516 C47,39.203523 43.138415,43.0650727 38.375,43.0650727 L9.625,43.0650727 C4.861585,43.0650727 1,39.203523 1,34.4401516 L1,22.9402569 C1,18.1768855 4.861585,14.3153359 9.625,14.3153359 L12.2056,14.3153359 L11.15519,8.0132786 C10.920245,6.60373651 11.87256,5.27055371 13.282115,5.03561087 C14.69167,4.80066802 16.024865,5.7529743 16.25981,7.16251639 L17.40981,14.0624532 C17.423955,14.1470924 17.43373,14.2315017 17.43948,14.3153359 L30.56052,14.3153359 C30.56627,14.2313867 30.576045,14.1470924 30.59019,14.0624532 L31.74019,7.16251639 C31.975135,5.7529743 33.30833,4.80066802 34.717885,5.03561087 Z M38.375,19.4902885 L9.625,19.4902885 C7.719565,19.4902885 6.175,21.0348394 6.175,22.9402569 L6.175,34.4401516 C6.175,36.3455692 7.719565,37.89012 9.625,37.89012 L38.375,37.89012 C40.280435,37.89012 41.825,36.3455692 41.825,34.4401516 L41.825,22.9402569 C41.825,21.0348394 40.280435,19.4902885 38.375,19.4902885 Z M14.8575,23.802749 C16.28649,23.802749 17.445,24.9612484 17.445,26.3902253 L17.445,28.6902043 C17.445,30.1191812 16.28649,31.2776806 14.8575,31.2776806 C13.42851,31.2776806 12.27,30.1191812 12.27,28.6902043 L12.27,26.3902253 C12.27,24.9612484 13.42851,23.802749 14.8575,23.802749 Z M33.1425,23.802749 C34.57149,23.802749 35.73,24.9612484 35.73,26.3902253 L35.73,28.6902043 C35.73,30.1191812 34.57149,31.2776806 33.1425,31.2776806 C31.71351,31.2776806 30.555,30.1191812 30.555,28.6902043 L30.555,26.3902253 C30.555,24.9612484 31.71351,23.802749 33.1425,23.802749 Z" id="&#x5F62;&#x72B6;&#x7ED3;&#x5408;" fill="#444444" fill-rule="nonzero"></path></g></svg>
-		  <div class="ai-title-text">${aiInterface.name}</div>
-		  <div class="ai-Toggle">切换简介</div>
-		  <div class="ai-speech-box">
-			<div class="ai-speech-content"></div>
-		  </div>
-		  <div class="ai-tag">${aiInterface.version}</div>
-		</div>
-		<div class="ai-explanation">${aiInterface.name}初始化中...</div>`;
-  
-	  // ai主体业务逻辑
-	  let animationRunning = true; // 标志变量，控制动画函数的运行
-	  let explanation = document.querySelector('.ai-explanation');
-	  let post_ai = document.querySelector('.post-ai');
-	  let ai_btn_item = document.querySelectorAll('.ai-btn-item');
-	  let ai_toggle = document.querySelector('.ai-Toggle');
-	  let ai_speech = document.querySelector('.ai-speech-box');
-	  let ai_str = '';
-	  let ai_str_length = '';
-	  let delay_init = 600;
-	  let i = 0;
-	  let j = 0;
-	  let speed = AI_option.speed || 20;
-	  let character_speed = speed*7.5;
-	  let sto = [];
-	  let elapsed = 0;
-	  let completeGenerate = false;
-	  let controller = new AbortController();//控制fetch
+
+function chucklePostAI(aiOption) {
+    main(aiOption);
+
+    if (aiOption.pjax) {
+        document.addEventListener('pjax:complete', () => {
+            setTimeout(() => {
+                main(aiOption);
+            }, 0);
+        });
+    }
+
+    function main(aiOption) {
+        // 删除现有的元素
+        const box = document.querySelector(".post-ai");
+        if (box) {
+            box.parentElement.removeChild(box);
+        }
+
+        const currentURL = window.location.href;
+
+        // 排除页面
+        if (aiOption.eliminate && aiOption.eliminate.length && aiOption.eliminate.some(item => currentURL.includes(item))) {
+            console.log("Post-Summary-AI 已排除当前页面(黑名单)");
+            return;
+        }
+        if (aiOption.whitelist && aiOption.whitelist.length && !aiOption.whitelist.some(item => currentURL.includes(item))) {
+            console.log("Post-Summary-AI 已排除当前页面(白名单)");
+            return;
+        }
+
+        // 获取挂载元素，即文章内容所在的容器元素
+        let targetElement = "";
+        // 若el配置不存在则自动获取，如果autoMount配置为真也自动获取
+        if (!aiOption.autoMount && aiOption.el) {
+            targetElement = document.querySelector(aiOption.el ? aiOption.el : '#post #article-container');
+        } else {
+            targetElement = getArticleElements();
+        }
+
+        if (!targetElement) {
+            return;
+        }
+
+        const aiInterface = {
+            name: "AI摘要",
+            introduce: "我是文章辅助AI, 用于生成本文简介。",
+            version: "GPT",
+            ...aiOption.interface
+        };
+
+        insertCSS(); // 插入css
+
+        // 插入html结构
+        const postAiBox = document.createElement('div');
+        postAiBox.className = 'post-ai';
+        postAiBox.innerHTML = `
+            <div class="ai-header">
+                <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24 4a20 20 0 100 40 20 20 0 000-40zm0 2.75a17.25 17.25 0 110 34.5 17.25 17.25 0 010-34.5z" fill="#444"/>
+                </svg>
+                <div class="ai-title-text">${aiInterface.name}</div>
+                <div class="ai-Toggle">切换简介</div>
+            </div>
+            <div class="ai-speech-box">
+                <div class="ai-speech-content"></div>
+            </div>
+            <div class="ai-tag">${aiInterface.version}</div>
+            <div class="ai-explanation">${aiInterface.name}初始化中...</div>
+        `;
+        targetElement.appendChild(postAiBox);
+
+        // ai主体业务逻辑
+        let animationRunning = true; // 标志变量，控制动画函数的运行
+        const explanation = document.querySelector('.ai-explanation');
+        const postAi = document.querySelector('.post-ai');
+        const aiBtnItem = document.querySelectorAll('.ai-btn-item');
+        const aiToggle = document.querySelector('.ai-Toggle');
+        const aiSpeech = document.querySelector('.ai-speech-box');
+        let aiStr = '';
+        let aiStrLength = '';
+        const delayInit = 600;
+        let i = 0;
+        let j = 0;
+        const speed = aiOption.speed || 20;
+        const characterSpeed = speed * 7.5;
+        const sto = [];
+        let elapsed = 0;
+        let completeGenerate = false;
+
+        const controller = new AbortController();
 	  let signal = controller.signal;
 	  let visitorId = ""; // 标识访客ID
 	  const summary_toggle = AI_option.summary_toggle ?? true;
