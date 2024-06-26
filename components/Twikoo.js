c4912772a5c22fa17f92a28841af290cf242e340
unique <1875760364@qq.com>
2024-06-26 15:43:29 +0800

    twikoo.init

--- components/Twikoo.js
+++ components/Twikoo.js
@@ -26,7 +26,7 @@ const Twikoo = ({ isDarkMode }) => {
         typeof twikoo.init === 'function'
       ) {
         twikoo.init({
-          envId: envId, // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+          envId: "https://twikoo.uniquecharm.online", // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
           el: el, // 容器元素
           lang: lang // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
           // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
