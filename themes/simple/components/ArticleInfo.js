186d89cdaa5da243dae57ecea345a7d7d7ada31c
unique <1875760364@qq.com>
2024-07-14 02:29:11 +0800

    1111

--- themes/simple/components/ArticleInfo.js
+++ themes/simple/components/ArticleInfo.js
@@ -26,14 +26,14 @@ export default function ArticleInfo (props) {
             <div className='flex flex-wrap text-gray-700 dark:text-gray-300'>
                 {post?.type !== 'Page' && (
                     <div className="space-x-3 mr-4" style="display: flex; align-items: center;">
-                        <span> <object type="image/svg+xml" data="/svg/xiaoxin.svg"/>:</span>
+                        <span><object type="image/svg+xml" data="/svg/xiaoxin.svg"></object>:</span>
                         <a href={siteConfig('SIMPLE_AUTHOR_LINK', null, CONFIG)}>{siteConfig('AUTHOR')}</a>
                         {post?.category && <span>  <i className="fa-regular fa-folder"></i> <a href={`/category/${post?.category}`} className="hover:text-red-400 transition-all duration-200">{post?.category}</a></span>}
                         {post?.tags && post?.tags?.length > 0 && post?.tags.map(t => <span style={{marginLeft:"0px"}} key={t}>/ <Link href={`/tag/${t}`}><span className=' hover:text-red-400 transition-all duration-200'>{t}</span></Link></span>)}
                     </div>)}
 
                 {post?.type !== 'Page' && (<div className=''>
-                    <span> <object type="image/svg+xml" data="/svg/fabu.svg"/>:</span>
+                    <span> <object type="image/svg+xml" data="/svg/fabu.svg"></object>:</span>
                         <Link
                             href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
                             passHref
