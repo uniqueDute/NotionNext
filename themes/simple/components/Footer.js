98bcbb4259f58a1325173207078a6b50ff2994b9
unique <1875760364@qq.com>
2024-07-14 14:25:38 +0800

    """""

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -1,5 +1,7 @@
 import DarkModeButton from '@/components/DarkModeButton'
 import { siteConfig } from '@/lib/config'
+import { useGlobal } from '@/lib/global'
+
 
 /**
  * 页脚
@@ -7,27 +9,28 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 export default function Footer(props) {
+  const { isDarkMode } = useGlobal()
+  const newStatus = !isDarkMode;
   const d = new Date()
   const currentYear = d.getFullYear()
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
-
   return (
-    <footer className="relative w-full px-6 border-t bg-white" id="footers">
+    <footer className="relative w-full px-6 border-t ${newStatus ? 'bg-white' : 'bg-black'}`}" id="footers">
       <DarkModeButton className="text-center pt-4" />
 
       <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-        <div className="text-center footer-text text-black"> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
-        <div className="md:p-0 text-center md:text-right text-xs footer-text text-black">
+        <div className="text-center footer-text ${newStatus ? 'text-white' : 'text-black'}`}"> &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights reserved.</div>
+        <div className="md:p-0 text-center md:text-right text-xs footer-text ${newStatus ? 'text-white' : 'text-black'}`}">
           {/* 右侧链接 */}
           {siteConfig('BEI_AN') && (
-            <a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4 footer-text">
+            <a href="https://beian.miit.gov.cn/" className=" dark:text-gray-200 no-underline hover:underline ml-4 footer-text ${newStatus ? 'text-white' : 'text-black'}`}">
               {siteConfig('BEI_AN')}
             </a>
           )}
-          <span className="no-underline ml-4 footer-text">
+          <span className="no-underline ml-4 footer-text ${newStatus ? 'text-white' : 'text-black'}`}">
             Powered by
-            <a href="https://github.com/tangly1024/NotionNext" className="hover:underline footer-text">
+            <a href="https://github.com/tangly1024/NotionNext" className="hover:underline footer-text ${newStatus ? 'text-white' : 'text-black'}`}">
               {' '}
               NotionNext {siteConfig('VERSION')}{' '}
             </a>
